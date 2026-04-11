aa71c8fef72c1f70e5ca82be77f2cfb2d67fc77c
arjpl <anon.ap.1224@gmail.com>
2026-04-11 17:34:35 +0530

    feat: add path parameters validation to delete method

--- books2.py
+++ books2.py
@@ -92,7 +92,7 @@ async def update_book(book: BookRequest):
             BOOKS[i] = book
             
 @app.delete("/books/id/{book_id}")
-async def delete_book(book_id: int):
+async def delete_book(book_id: int = Path(ge=0)):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
